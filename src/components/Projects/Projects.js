082b2a2ea2ed385e7e7442cda03019c8872f38cc
Code masters <pankajhadole24@gmail.com>
2024-03-21 23:55:44 +0530

    Added new working project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-// import leaf from "../../Assets/Projects/leaf.png";
+import maryway from "../../Assets/Projects/maryway.jpg";
 import emotion from "../../Assets/Projects/emotion.png";
 // import editor from "../../Assets/Projects/codeEditor.png";
 // import chatify from "../../Assets/Projects/chatify.png";
@@ -53,38 +53,28 @@ function Projects() {
               demoLink="https://editor.soumya-jit.tech/"              
             />
           </Col> */}
-
 <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition Based Attendance System"
-              description="The Facial Recognition Based Attendance System uses advanced technology to quickly and accurately track student attendance. It captures and analyzes facial features, matches them with stored student images, and records attendance automatically. It's user-friendly, secure, and scalable for implementation in educational institutions."
-              ghLink="https://github.com/PHCoder05/Face-Recognition-Base-Attedance-System"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
-            />
-          {/* </Col>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
-            />
-          </Col> */}
+    <ProjectCard
+        imgPath={emotion}
+        isBlog={false}
+        title="Face Recognition Based Attendance System"
+        description="The Facial Recognition Based Attendance System uses advanced technology to quickly and accurately track student attendance. It captures and analyzes facial features, matches them with stored student images, and records attendance automatically. It's user-friendly, secure, and scalable for implementation in educational institutions."
+        ghLink="https://github.com/PHCoder05/Face-Recognition-Base-Attedance-System"
+        demoLink="YOUR_DEMO_LINK_HERE"
+    />
+</Col>
+<Col md={4} className="project-card">
+    <ProjectCard
+        imgPath={maryway}
+        isBlog={false}
+        title="Markyway"
+        description=
+        "This is a social media marketing website. I started as an intern and was later hired as a Website Administrator. I developed and now maintain the website part-time."
+        ghLink="https://github.com/PHCoder05/Markyway"
+        demoLink="https://markyway.netlify.app/"
+    />
+</Col>
 
-          {/* <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"  
-            />*/}
-          </Col> 
 
         </Row>
       </Container>
